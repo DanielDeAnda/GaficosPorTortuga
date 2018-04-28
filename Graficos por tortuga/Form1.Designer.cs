7adf5d965bad0ab4c2298238254c132fab230007
DanielDeAnda <36206005+DanielDeAnda@users.noreply.github.com>
2018-04-28 15:30:01 -0500

    GraficosPorTortuga

---
+++ Graficos por tortuga/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Graficos_por_tortuga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.txtTablero = new System.Windows.Forms.TextBox();
+            this.txtAvanzar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(13, 43);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(13, 73);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(154, 42);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(154, 72);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // txtTablero
+            // 
+            this.txtTablero.Location = new System.Drawing.Point(13, 120);
+            this.txtTablero.Multiline = true;
+            this.txtTablero.Name = "txtTablero";
+            this.txtTablero.Size = new System.Drawing.Size(235, 118);
+            this.txtTablero.TabIndex = 5;
+            // 
+            // txtAvanzar
+            // 
+            this.txtAvanzar.Location = new System.Drawing.Point(154, 16);
+            this.txtAvanzar.Name = "txtAvanzar";
+            this.txtAvanzar.Size = new System.Drawing.Size(100, 20);
+            this.txtAvanzar.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtAvanzar);
+            this.Controls.Add(this.txtTablero);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox txtTablero;
+        private System.Windows.Forms.TextBox txtAvanzar;
+    }
+}
+
---
+++ Graficos por tortuga/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graficos_por_tortuga
+{
+    public partial class Form1 : Form
+    {
+        Tortuga Tablero = new Tortuga();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbajo_Click(object sender, EventArgs e)
+        {
+            Tablero._pluma1 = -1;
+        }
+
+        private void btnArriva_Click(object sender, EventArgs e)
+        {
+            Tablero._pluma1 = 1;
+        }
+
+        private void btnGiraDerecha_Click(object sender, EventArgs e)
+        {
+            Tablero.GirarDerecha();
+        }
+
+        private void btnGiraIzquierda_Click(object sender, EventArgs e)
+        {
+            Tablero.GirarIzquierda();
+        }
+
+        private void btnAvanza_Click(object sender, EventArgs e)
+        {
+            Tablero.Avanza(Convert.ToInt32(txtAvanzar.Text));
+        }
+
+        private void btnMostrarArreglo_Click(object sender, EventArgs e)
+        {
+            txtTablero.Clear();
+            Tablero.Imprime();
+            txtTablero.Text = Tablero.ToString();
+        }
+    }
+}
